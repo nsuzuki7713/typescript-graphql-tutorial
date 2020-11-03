da02e06515f38805f43ad41b4a798933e066d1ef
nsuzuki7713 <nsuzuki7713@gmail.com>
2020-11-03 21:53:58 +0900

    プロジェクトを作成する

---
+++ README.md
@@ -0,0 +1,3 @@
+# 【Typescript】GraphQL入門
+
+[参照記事](https://zenn.dev/nekoniki/books/e2f5be82d54e5acac306)
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export type Chapter = {
+  no: number;
+  name: string;
+  version: string;
+  original: boolean;
+  postDate: string;
+}
