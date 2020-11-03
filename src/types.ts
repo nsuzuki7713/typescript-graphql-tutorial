9dc6cd2b9c96a5e6a1de03e5616d2d679f7afa7a
nsuzuki7713 <nsuzuki7713@gmail.com>
2020-11-03 23:22:33 +0900

    Mutationを用いてデータの追加・更新を行う

--- src/types.ts
+++ src/types.ts
@@ -9,3 +9,19 @@ export type Chapter = {
 export type ChapterParam = {
   no: number;
 }
+
+export type CreateParam = {
+  no: number;
+  name: string;
+  version: string;
+  original: boolean;
+  postDate: string;
+}
+
+export type UpdateParam = {
+  no: number;
+  name: string;
+  version: string;
+  original: boolean;
+  postDate: string;
+}
